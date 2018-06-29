2ce29f54704844a80f92c185cd1e67be0e1a6fee
Janice Childers <janice.childers@ZIRMED.COM>
2018-06-29 13:04:29 -0400

    Outlining customer info object in model, setting up view

---
+++ ProtoXmlSerializer/Models/CustInfoSerialize.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Models
+{
+    public class CustInfoSerialize
+    {
+        public string ErrorMessage { get; set; }
+
+        public CustInfoSerialize(int id, string firstName, string lastName, string streetAddress, string city, string state, int zip)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            StreetAddress = streetAddress;
+            City = city;
+            State = state;
+            ZipCode = zip;
+        }
+
+        public int Id { get; set; }
+
+        [Display(Name = "First name"), Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name"), Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street address"), Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Display(Name = "Zip code"), Required, MinLength(5, ErrorMessage = "Zip code must be at least five digits.")]
+        public int ZipCode { get; set; }
+
+        public string Serialized { get; set; }
+
+    }
+}
---
+++ ProtoXmlSerializer/Objects/CustomerInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProtoBuf;
+
+namespace ProtoXmlSerializer.Objects
+{
+    [ProtoContract]
+    public class CustomerInfo
+    {
+        public CustomerInfo()
+        {
+            //whatever you need for object here
+        }
+
+        //[ProtoMember(1)]
+        
+        //get; set; items making up object
+    }
+}
